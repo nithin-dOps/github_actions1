88bc9ebd5fc0e281b09ee7494472e4662a45f57f
nithin-dOps <bandanithinreddy@gmail.com>
2025-01-25 20:20:35 +0000

    space changes between operators in .py file

--- file_test.py
+++ file_test.py
@@ -2,17 +2,17 @@
 
 def test_calc_addition():
   # Function test the output of 2+4
-    output = 2+4
+    output = 2 + 4
     assert output == 6
 
 def test_calc_substraction():
   # Function test the output of 2-4
-    output = 2-4
+    output = 2 - 4
     assert output == -2
 
 def test_calc_multiply():
   # Function test the output of 2*4
-    output = 2*4
+    output = 2 * 4
     assert output == 8
 
 def test_coucou():
